ed951ea93253c8e06b1089137b4f71b48ecea9a8
yang-wei <yangweilim21@gmail.com>
2021-02-26 22:18:12 +0900

    git init

---
+++ src/main/java/com/ml/featureswitch/FeatureswitchApplication.java
@@ -0,0 +1,13 @@
+package com.ml.featureswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureswitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureswitchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ml/featureswitch/FeatureswitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.featureswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureswitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
